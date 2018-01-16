4de9435b17e2dc458fefd7edd30878d7e44ea543
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-01-16 16:52:02 -0700

    second push

--- src/components/Create/Create_wed.js
+++ src/components/Create/Create_wed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
-import './Create.css'
+import './Create.css';
+import axios from 'axios'
 export default class Create_wed extends Component {
     constructor(){
         super();
@@ -22,6 +23,21 @@ export default class Create_wed extends Component {
         this.setState({
             [key]: val
         })
+    }
+
+    submit(){
+        let body = {
+            groom_name: this.state.groomName,
+            bride_name: this.state.brideName,
+            g_phone: this.state.groomPhone,
+            b_phone: this.state.bridePhone,
+            dates: this.state.date,
+            time_frame: this.state.time,
+            venue: this.state.venue
+        }
+
+        let promise = axios.post("/api/wedding", body)
+        promise.then(({data})=> this.props.history.push('/event/' +data.id))
     }
 
     render(){
